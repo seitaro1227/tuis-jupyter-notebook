eb8dabecd97da531a3edfda7b7d4d2c93fd46a79
hirakawa_s <hirakawa_s@welmo.co.jp>
2017-11-20 11:32:33 +0900

    typo修正

--- 02_setup_jnb.sh
+++ 02_setup_jnb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pip install --require requirements.txt
+pip install --requirement requirements.txt
 jupyter notebook --generate-config
 echo "~/.jupyter/jupyter_notebook_config.pyに"
 echo "パスワードを記載してください"
