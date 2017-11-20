8d42c416d98d52e64b2e632b2e4786e13749e655
hirakawa_s <hirakawa_s@welmo.co.jp>
2017-11-20 11:58:38 +0900

    パスワードの設定をhash形式に変更

--- 02_setup_jnb.sh
+++ 02_setup_jnb.sh
@@ -2,6 +2,15 @@
 
 pip install --requirement requirements.txt
 jupyter notebook --generate-config
+echo "パスワードを2回入力してください"
+python -c 'from notebook.auth import passwd;print(passwd())'
+echo "↑をコピーして"
 echo "~/.jupyter/jupyter_notebook_config.pyに"
-echo "パスワードを記載してください"
-echo "c.NotebookApp.token = 'パスワード'"
+echo "こんなかんじに貼り付けてください"
+echo "c.NotebookApp.password = 'sha1:〜〜〜〜〜〜'"
+
+echo "Homeフォルダの表示方法は"
+echo "c9の歯車アイコンをクリックして"
+echo "Show Hoem in Favorites"
+echo "Show Hidden Files"
+echo "にチェックを入れてください。"
